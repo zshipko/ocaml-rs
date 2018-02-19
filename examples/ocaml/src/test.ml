975b6617b13b60bcb249f5ecb7da7a0660d494a9
Zach Shipko <zachshipko@gmail.com>
2018-02-18 23:47:21 -0800

    Cleanup example OCaml code

--- examples/ocaml/src/test.ml
+++ examples/ocaml/src/test.ml
@@ -15,19 +15,38 @@ let _ =
     Callback.register "print_testing" print_testing
 
 let _ =
-  let string = "string thing" in
-  let deadbeef = 0xdeadbeef in
-  let res = send_int 0xb1b1eb0b in
-  Printf.printf "send_int returned: 0x%x\n" res;
-  flush stdout;
-  send_two deadbeef string;
-  send_two (f deadbeef) string;
-  let res = send_tuple (1, 2) in
-  Printf.printf "%d\n" res;
-  let (a, b, c) = new_tuple () in
-  Printf.printf "%d %d %d\n" a b c;
-  let arr = new_array () in
-  Array.iter (Printf.printf "%d\n") arr;
-  let lst = new_list () in
-  List.iter (Printf.printf "%d\n") lst;
-  testing_callback 5 10
+
+    (* send_int *)
+    let string = "string thing" in
+    let deadbeef = 0xdeadbeef in
+    let res = send_int 0xb1b1eb0b in
+    Printf.printf "send_int returned: 0x%x\n" res;
+    flush stdout;
+    assert (res = 0xbeef);
+
+    (* send_two *)
+    send_two deadbeef string;
+    send_two (f deadbeef) string;
+
+    (* send_tuple *)
+    let res = send_tuple (1, 2) in
+    Printf.printf "%d\n" res;
+    assert (res = 3);
+
+    (* new_tuple *)
+    let (a, b, c) = new_tuple () in
+    Printf.printf "%d %d %d\n" a b c;
+    assert (a = 0 && b = 1 && c = 2);
+
+    (* new_array *)
+    let arr = new_array () in
+    Array.iter (Printf.printf "%d\n") arr;
+    assert (arr = [| 0; 1; 2; 3; 4 |]);
+
+    (* new list *)
+    let lst = new_list () in
+    List.iter (Printf.printf "%d\n") lst;
+    assert (lst = [0; 1; 2; 3; 4]);
+
+    (* testing_callback *)
+    testing_callback 5 10
