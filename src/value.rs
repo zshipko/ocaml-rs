415cd00579f0111aafc4d2c6a72513aebf89a0e3
Zach Shipko <zachshipko@gmail.com>
2019-03-22 20:04:45 -0700

    Wrap value allocation functions in caml_frame

--- src/value.rs
+++ src/value.rs
@@ -66,34 +66,36 @@ pub const UNIT: Value = Value(core::mlvalues::UNIT);
 impl Value {
     /// Allocate a new value with the given size and tag.
     pub fn alloc(n: usize, tag: Tag) -> Value {
-        caml_local!(x);
-        x.0 = unsafe { core::alloc::caml_alloc(n, tag.into()) };
-        return x;
+        caml_frame!(|x| {
+            x.0 = unsafe { core::alloc::caml_alloc(n, tag.into()) };
+            x
+        })
     }
 
     /// Allocate a new tuple value
     pub fn alloc_tuple(n: usize) -> Value {
-        caml_local!(x);
-        x.0 = unsafe { core::alloc::caml_alloc_tuple(n) };
-        return x;
+        caml_frame!(|x| {
+            x.0 = unsafe { core::alloc::caml_alloc_tuple(n) };
+            x
+        })
     }
 
     /// Allocate a new small value with the given size and tag
     pub fn alloc_small(n: usize, tag: Tag) -> Value {
-        caml_local!(x);
-        x.0 = unsafe { core::alloc::caml_alloc_small(n, tag.into()) };
-        return x;
+        caml_frame!(|x| {
+            x.0 = unsafe { core::alloc::caml_alloc_small(n, tag.into()) };
+            x
+        })
     }
 
     /// Allocate a new value with a custom finalizer
     pub fn alloc_custom<T>(value: T, finalizer: extern "C" fn(core::Value)) -> Value {
-        caml_local!(x);
-        unsafe {
-            x.0 = core::alloc::caml_alloc_final(mem::size_of::<T>(), finalizer, 0, 1);
+        caml_frame!(|x| {
+            x.0 = unsafe { core::alloc::caml_alloc_final(mem::size_of::<T>(), finalizer, 0, 1) };
             let ptr = x.custom_ptr_val_mut::<T>();
-            ptr::write(ptr, value);
-        }
-        x
+            unsafe { ptr::write(ptr, value) };
+            x
+        })
     }
 
     pub fn set_custom<T>(&mut self, value: T) -> T {
