8a8c8a1c9db1fab6351a9d9a411c4e81f12386ff
Zach Shipko <zachshipko@gmail.com>
2020-04-12 21:35:03 -0700

    Fix name

--- example/src/enums.rs
+++ example/src/enums.rs
@@ -1,41 +1,41 @@
 use ocaml::{FromValue, ToValue};
 
 #[derive(ToValue, FromValue)]
-enum Enum11 {
+enum Enum1 {
     Empty,
     First(ocaml::Int),
     Second(ocaml::Array<&'static str>),
 }
 
 #[ocaml::func]
-pub fn enum1_empty() -> Enum11 {
-    Enum11::Empty
+pub fn enum1_empty() -> Enum1 {
+    Enum1::Empty
 }
 
 #[ocaml::func]
-pub fn enum1_first(i: ocaml::Int) -> Enum11 {
-    Enum11::First(i)
+pub fn enum1_first(i: ocaml::Int) -> Enum1 {
+    Enum1::First(i)
 }
 
 #[ocaml::func]
-pub fn enum1_make_second(s: &'static str) -> Enum11 {
+pub fn enum1_make_second(s: &'static str) -> Enum1 {
     let mut arr = ocaml::Array::alloc(1);
     let _ = arr.set(0, s);
-    Enum11::Second(arr)
+    Enum1::Second(arr)
 }
 
 #[ocaml::func]
-pub fn enum1_get_second_value(e: Enum11) -> Option<ocaml::Array<&'static str>> {
+pub fn enum1_get_second_value(e: Enum1) -> Option<ocaml::Array<&'static str>> {
     match e {
-        Enum11::Second(x) => Some(x),
-        Enum11::Empty | Enum11::First(_) => None,
+        Enum1::Second(x) => Some(x),
+        Enum1::Empty | Enum1::First(_) => None,
     }
 }
 
 #[ocaml::func]
-pub fn enum1_is_empty(e: Enum11) -> bool {
+pub fn enum1_is_empty(e: Enum1) -> bool {
     match e {
-        Enum11::Empty => true,
+        Enum1::Empty => true,
         _ => false,
     }
 }
