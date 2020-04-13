7ba372eec14937e4280b88d3bb80cde377336b67
Zach Shipko <zachshipko@gmail.com>
2020-04-12 21:06:11 -0700

    Rename ocamlopt -> compiler

--- src/lib.rs
+++ src/lib.rs
@@ -137,7 +137,7 @@ pub type Int = sys::mlvalues::Intnat;
 pub type Uint = sys::mlvalues::Uintnat;
 
 /// Wraps `sys::COMPILER` as `std::process::Command`
-pub fn ocamlopt() -> std::process::Command {
+pub fn compiler() -> std::process::Command {
     std::process::Command::new(sys::COMPILER)
 }
 
