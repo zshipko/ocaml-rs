f46893ca07c9f36e9fa47bba722434c7c9dd8d70
Łukasz Kurowski <crackcomm@gmail.com>
2021-03-31 04:52:37 +0200

    Use default quota for benchmarks

--- test/src/bench.ml
+++ test/src/bench.ml
@@ -2,7 +2,6 @@ open Core_bench
 open Ocamlrs_test
 
 let () =
-  let run_config = Bench.Run_config.create ~quota:(Bench.Quota.Num_calls 1000) () in
-  Bench.bench ~run_config
+  Bench.bench
     [ Bench.Test.create ~name:"func" Runtime.func
     ; Bench.Test.create ~name:"native_func" Runtime.native_func ]
